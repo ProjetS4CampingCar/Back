b31bf2f1348f09fa76fea8aa5c5526630cd285f9
SenseiJo <jonathan.tcha@gmail.com>
2024-01-29 16:55:44 +0100

    Mis en place du serveur

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const socketIo = require("socket.io");
+
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (req, res) => {
+  res.send("Bonjour ca marche heu peu etre");
+});
+
+const PORT = 3008;
+server.listen(PORT, () => {
+  console.log("le serveur est bien sur le port" + PORT);
+});
