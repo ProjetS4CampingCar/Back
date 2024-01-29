5ae871405ddd967d19a17efe010b255ad0f9c087
SenseiJo <jonathan.tcha@gmail.com>
2024-01-29 17:31:17 +0100

    Test de connection de la BDD

--- src/db/sequelize/sequelize.js
+++ src/db/sequelize/sequelize.js
@@ -1,6 +1,11 @@
-const { Sequelize, DataType } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
+const materialModel = require("../../modele/material");
 
-const sequelize = new Sequelize("info8", "inf8", "Q3P", {
+const sequelize = new Sequelize("info8", "info8", "Q3P", {
   dialect: "mysql",
   host: "localhost",
 });
+
+//const material = materialModel(sequelize, DataTypes);
+
+sequelize.sync().then(console.log("La connection avec la base marche bien"));
---
+++ src/modele/material.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "material",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "Material",
+    }
+  );
+};
