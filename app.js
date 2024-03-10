a6b15c9e7f22aa7b48f6a1a96a98251a6606afde
Aekhy <102717242+Aekhy@users.noreply.github.com>
2024-03-10 14:18:56 +0100

    re-fixed wip

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const socketIo = require("socket.io");
 const sequelize = require("./src/db/sequelize/sequelize");
-<<<<<<< HEAD
 const materialRouter = require("./src/router/materialRouter");
 const reservationRouter = require("./src/router/reservationRouter");
 const bodyParser = require("body-parser");
-=======
 const router = require("./src/router/allRouter");
 
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 
 const cors = require("cors");
->>>>>>> auth-user
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
@@ -28,17 +25,14 @@ app.get("/", (req, res) => {
   res.json("Commande Pour lancer NODE JS est (npx nodemon app.js)");
 });
 
-<<<<<<< HEAD
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(pathAPI, materialRouter);
 app.use(pathAPI, reservationRouter);
 
-=======
 //app.use(pathAPI, materialRouter);
 app.use(pathAPI, router);
->>>>>>> auth-user
 
 const PORT = 3008;
 server.listen(PORT, () => {
